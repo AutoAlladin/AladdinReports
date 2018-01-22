8958e1fe6be0817ad08c4eb15a048ae0bcf7ac89
Autoaladdin <qa@aps-trade.com>
2018-01-22 16:04:52 +0200

    result from db

--- test1.py
+++ test1.py
@@ -7,21 +7,26 @@ import gridfs
 client = MongoClient('192.168.80.121', 27017)
 db = client['aladdin_tests']
 test_params = db["test_params"]
-test_result = db["test_reGsults"]
+test_result = db["test_results"]
 print(test_params)
 
 query = {"_id" : ObjectId("5a659573f776481ff019bfd6")}
-result = test_result.find_one(query)
+db_result = test_result.find_one(query)
 
-print(result)
 
-with(open('templates\\new 1.html', mode="r", encoding="UTF8")) as f:
+with(open('templates\\new 1.html', mode="r+", encoding="UTF8")) as f:
     file1 = f.read()
-print(file1)
 
-s = Template('$_id')
-print(s.substitute(_id = 654654654))
 
+s = Template(file1)
+base_info = dict(
+    _id=db_result["_id"],
+    test_name=db_result["test_name"],
+    test_timestamp=db_result["test_timestamp"],
+    test_result=db_result["test_result"]
+)
+print(s.substitute(base_info))
+# print(s.substitute(file1))
 
 # with(open('warfile.txt', mode="w", encoding="UTF8")) as f:
 #     f.write(str(result))
